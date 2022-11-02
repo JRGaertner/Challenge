acd9a949b3c1e36a7446f705113a49626dc73e9e
Jared Gaertner <jared.gaertner@hcss.com>
2022-11-02 01:33:29 -0500

    use css module

--- pages/Page2.tsx
+++ pages/Page2.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
+import styles from '../styles/Page2.module.css'
 
 export default function Page2() {
   return (
     <>
+      <div className={styles.bruh}>
+        <Image src='/valant.png' alt='bruh' width='459' height='287' />
+      </div>
       <h1>Picture</h1>
-      <Image src='/valant.png' alt='bruh' width='459' height='287' />
     </>
   )
 }
